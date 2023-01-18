beaef0bd355c95501ac275969bb785d8ac110225
Tanay0708 <123vyast@gmail.com>
2023-01-18 21:24:14 +0530

    added day2

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import ProductList from './Components/ProductList';
 
 function App() {
   return (
     <div className="App">
       HELLO THIS MYNTRA WEBSITE
+      <ProductList />
     </div>
   );
 }
