78b1667a83495a9091771f9bb6b5d9eac14b0fc6
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2023-01-18 23:34:20 +0530

    addedsignuppagesettings

--- myntra/src/Redux/Auth/UserSignup/usergetdataaction.js
+++ myntra/src/Redux/Auth/UserSignup/usergetdataaction.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 import * as types from "./usergetdataactiontype"
 const usergetdataaction = ()=>(dispatch) => {
     dispatch({type:types.GETUSERDATAREQUEST})
-    return axios.get(`https://appletvusers.onrender.com/users`)
-    .then(r=>dispatch({type:types.GETUSERDATASUCCESS,payload:r.data}))
+    return axios.get("https://awful-fly-shoulder-pads.cyclic.app/user_signup")
+    .then((r)=>{dispatch({type:types.GETUSERDATASUCCESS,payload:r.data})})
     .catch(err=>dispatch({type:types.GETUSERDATAFAILURE}))
 }
 
