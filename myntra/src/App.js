2913120ba4ada56abc5a645a430a70c4b8a28404
Navneet <navneetburman900@gmail.com>
2023-01-19 21:16:24 +0530

    day-3

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import Navbar from './components/Navbar';
+import Navbar from './Components/Navbar';
 import AllRoutes from './pages/AllRoutes';
 
 import ProductList from './Components/ProductList';
--- myntra/src/Redux/store.js
+++ myntra/src/Redux/store.js
@@ -1,5 +1,6 @@
 import {applyMiddleware, legacy_createStore} from "redux";
-import { reducer } from './Products/reducer';
+import { reducer as productDatareducer } from './Products/reducer';
+import { combineReducers } from "redux";
 import thunk from 'redux-thunk';
-
-export const store=legacy_createStore(reducer,applyMiddleware(thunk));
+const rootReducer=combineReducers({productDatareducer})
+export const store=legacy_createStore(rootReducer,applyMiddleware(thunk));
