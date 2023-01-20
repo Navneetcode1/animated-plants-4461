5fdc9ba6c22950594d908f8768c1a65e94ee66bd
Navneet <navneetburman900@gmail.com>
2023-01-20 21:30:37 +0530

    day-4

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -6,13 +6,14 @@ import Navbar from './Components/Navbar';
 import AllRoutes from './pages/AllRoutes';
 // import ProductList from './Components/ProductList';\
 import ProductList from "./Components/CompnentsT/ProductList";
+import Navbar2 from "./Components/Navbar2";
 
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar2 />
       <AllRoutes />
       <ProductList />
     </div>
