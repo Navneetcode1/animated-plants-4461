75432deeafa32e923178be0de07b3ceb120dbf49
Navneet <navneetburman900@gmail.com>
2023-01-21 22:02:15 +0530

    day-5

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -4,13 +4,13 @@ import "./App.css";
 
 
 // import Navbar from './Components/Navbar';
-
+ 
 import AllRoutes from './pages/AllRoutes';
 // import ProductList from './Components/ProductList';\
 import ProductList from "./Components/CompnentsT/ProductList";
 import Navbar2 from "./Components/Navbar2";
 
-import ProductList from './Components/ProductList';
+// import ProductList from './Components/ProductList';
 import Navbar from './Components/Navbar';
 
 
