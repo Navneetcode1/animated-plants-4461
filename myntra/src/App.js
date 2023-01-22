390bbc054a258fb7f22f40b9783f9015b9735a8e
Vivek Patel <vp133202@gmail.com>
2023-01-22 18:59:18 +0530

    fdsf

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -12,12 +12,15 @@ import ProductDetail from "./PagesT/ProductDetail";
 import Filters from "./Components/CompnentsT/Filters";
 import Navbar2 from "./Components/Navbar2";
 import Navbar from './Components/Navbar';
+import Home from "./VPages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar2 />
+      <Home/>
       <AllRoutes />
+    
   
       <ProductList />
       {/* <ProductDetail />  */}
